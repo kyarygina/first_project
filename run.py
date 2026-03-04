691991db263536226798d045620eee70b4844f64
k.yarygina <ks.yarygina@yandex.ru>
2026-03-04 21:58:22 +0400

    В run-файл добавлена возможность запуска через GUI

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from file_manager.cli import main
+import sys
+from file_manager.cli import main as cli_main
+from file_manager.gui import run_gui
+
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1 and sys.argv[1] == "gui":
+        run_gui()
+    else:
+        cli_main()
